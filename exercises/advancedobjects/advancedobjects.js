0d60c118e5bdbab9306d943a2f386ac220d68d04
Carisa Brown <rainbeamer@gmail.com>
2018-03-04 18:20:56 -0500

    add advanced objects exercise

---
+++ exercises/advancedobjects/advancedobjects.js
@@ -0,0 +1,24 @@
+
+
+var warriorFactory = function(name, weapon){
+  var warrior = {};
+
+  warrior.name = name;
+  warrior.weapon = weapon;
+
+  warrior.attack = function(){
+    return this.name + " attacks with their " + this.weapon + "!";
+  };
+
+  warrior.adventure = function(){
+    return "Behold, the great warrior " + this.name + ", foremost of all warriors. They draw forth to face the Dragon of Kalamazar. Watch as " + this.attack() + ". Such bravery!";
+  };
+  
+  $("#warriortarget").append('<p>'+warrior.adventure()+'</p>');
+  $("#warriortarget").append('<p>'+warrior.attack()+'</p>');
+S
+  return warrior;
+
+};
+
+warriorFactory( "Olav the Destroyer of Weaklings", "Spiked Deathmaul of Lamentation");
