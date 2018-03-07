605c3aec3bfc8373317817066ccf29f251fa5fae
Carisa Brown <rainbeamer@gmail.com>
2018-03-07 12:43:14 -0500

    update links and add content info

---
+++ blog/ajaxpage.js
@@ -0,0 +1,11 @@
+var getPosts = $.ajax( "https://jsonplaceholder.typicode.com/posts" );
+var handlePost = function( post ){
+    var title = "<h3>" + post.title + "</h3>";
+    var content = "<p>" + post.body + "</p>";
+    var blurb = "<div class='post'>" + title + content + "</div>";
+  
+    $( ".content" ).append( blurb );
+}
+
+getPosts.then(
+    ( posts ) => posts.forEach( handlePost ));
